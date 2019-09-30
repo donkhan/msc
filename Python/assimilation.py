2bc0f7d7f4d67768197dbb2148b0b640a67be579
donkhan <kamilkhan@rediffmail.com>
2019-09-30 14:52:31 +0530

    Create assimilation.py
    .

---
+++ Python/assimilation.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  4 11:27:37 2019
+
+@author: 1msc11
+"""
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+print(letters[-3:-1])
+
+
+mylist = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+print(mylist[18])
+
+mylist = ["Jaswant","Raj"]
+mylist.append("John")
+
+print(mylist)
+
+list1 = [1.23,1.45,1.02]
+list2 = [1.99]
+
+list2.append(float(list1[2]))
+
+print(list2)
+
+c = mylist[0] + mylist[-1]
+print(c)
+
