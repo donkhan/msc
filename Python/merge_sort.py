311581f16d666721a29517f3f4c955c38bbfffb9
donkhan <kamilkhan@rediffmail.com>
2019-10-01 16:27:25 +0530

    .

--- Python/merge_sort.py
+++ Python/merge_sort.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Oct  1 15:48:11 2019
-sort([2,1,4,5,6,3,7,9,23,34,145])
-print(sort([2,1,4,5]))
 @author: 1msc11
 """
 
@@ -10,9 +8,8 @@ print(sort([2,1,4,5]))
 def merge(list_1,list_2):
     l1 = len(list_1)
     l2 = len(list_2)
-    i = 0
+    i = 0 
     j = 0
-    k = 0
     l = []
     while(i < l1 and j < l2):
         if list_1[i] <  list_2[j]:
@@ -21,13 +18,13 @@ def merge(list_1,list_2):
         else:
             l.append(list_2[j])
             j = j + 1
-        k = k + 1
     while(i < l1):
         l.append(list_1[i])
         i = i + 1
     while(j < l2):
         l.append(list_2[j])
         j = j + 1
+    print(str(l))
     return l
 
 def sort(l):
