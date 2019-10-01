b3878c14679c177b1f46222cdcaa0a0b6572e885
donkhan <kamilkhan@rediffmail.com>
2019-10-01 15:28:34 +0530

    Done.Tested with test case 3

--- DS/GraphTheory/Prims.c
+++ DS/GraphTheory/Prims.c
@@ -12,6 +12,69 @@ struct edge{
 	int cost;
 };
 
+void print_line();
+void print_edges(struct edge *edges,int vc);
+void fill(char* set,int vc);
+void empty(char *set,int vc);
+void add_to_set(char *set,char vertex,int vc);
+void remove_from_set(char *set,char vertex,int vc);
+void print_set(char *set_name,char *set,int vc);
+void add_to_mst(struct edge *mst,struct edge *edge,int pos);
+
+struct edge* find_distance(char src,char dst,struct edge* edges,int ec,int INFINITE);
+
+
+void prims(struct edge *edges,int vc,int ec){
+	char *reached_set = (char *)malloc(sizeof(char) * vc);
+	char *unreached_set = (char *)malloc(sizeof(char) * vc);
+	struct edge *mst = (struct edge *)malloc(sizeof(struct edge *)*ec);
+	struct edge *t_edge;
+	struct edge *x_edge;
+	int no_of_edges_of_given_vertex;
+	int INFINITE = 1000,pass = 0,pos = 0,i,j,d,sd;
+	int srs = 0,snrs = vc;
+	char next_vertex,next_possible_vertex;
+	clrscr();
+	fill(unreached_set,vc);
+	empty(reached_set,vc);
+	clrscr();
+
+	next_vertex = VERTEX_START;
+	while(snrs > 0 && pass < 10){
+		printf("Pass %d \n",pass+1);
+		snrs = snrs - 1;
+		srs = srs + 1;
+		remove_from_set(unreached_set,next_vertex,vc);
+		add_to_set(reached_set,next_vertex,vc);
+		print_set("U-V",unreached_set,vc);
+		print_set("U  ",reached_set,vc);
+		sd = INFINITE;
+		next_possible_vertex = next_vertex;
+		for(i = 0;i<vc;i++){
+			for(j = 0;j<vc;j++){
+				if(unreached_set[j] != ' ' && reached_set[i] != ' '){
+					t_edge = find_distance(unreached_set[j],
+						reached_set[i],edges,ec,1000);
+					if(t_edge->cost < sd){
+						sd = t_edge->cost;
+						x_edge = t_edge;
+						next_possible_vertex = unreached_set[j];
+					}
+				}
+			}
+		}
+		add_to_mst(mst,x_edge,pos++);
+		next_vertex = next_possible_vertex;
+		pass = pass + 1;
+		printf("MST as of now \n");
+		print_edges(mst,pos);
+		getch();
+		clrscr();
+	}
+	printf("Final MST \n");
+	print_edges(mst,pos-1);
+}
+
 void print_line(){
 	printf("----------------------------------------------------\n");
 }
@@ -24,63 +87,13 @@ void print_edges(struct edge *edges,int ec){
 	print_line();
 }
 
-
-void add_to_sparse_matrix(struct edge edge,int pos, struct edge *sparse_matrix){
-	sparse_matrix[pos] = edge;
+void add_to_mst(struct edge *mst,struct edge *edge,int pos){
+	struct edge e;
+	e.src = edge->src;
+	e.dst = edge->dst;
+	e.cost = edge->cost;
+	mst[pos] = e;
 }
-
-
-
-struct edge* get_edges_of_vertex(struct edge* edges,char vertex,int ec){
-	int count = 0,i=0,j = 0;
-	struct edge* edges_of_vertex;
-	for(i = 0;i<ec;i++){
-		if(edges[i].src == vertex || edges[i].dst == vertex){
-			count = count + 1;
-		}
-	}
-	edges_of_vertex = (struct edge *) malloc(sizeof(struct edge *) * count);
-	for(i = 0;i<ec;i++){
-		if(edges[i].src == vertex || edges[i].dst == vertex){
-			edges_of_vertex[j] = edges[i];
-			j = j + 1;
-		}
-	}
-	return edges_of_vertex;
-}
-
-void sort(struct edge* edges,int ec){
-	int i,j;
-	struct edge t;
-	for(i = 0;i<ec -1;i++){
-		for(j = i+1;j<ec;j++){
-			if(edges[i].cost > edges[j].cost){
-				t = edges[i];
-				edges[i] = edges[j];
-				edges[j] = t;		
-			}
-		}
-	}
-}
-
-int get_no_of_edges(struct edge* edges){
-	int i = 0;
-	while(1){
-		if(edges[i].src >= VERTEX_START && edges[i].src <=  VERTEX_END){
-			i = i + 1;
-		}else{
-			break;
-		}
-	}
-	return i;
-	
-}
-
-
-int get_array_value(char vertex){
-	return vertex - VERTEX_START;
-}
-
 
 void fill(char *array,int vc){
 	int i = 0;
@@ -94,26 +107,6 @@ void empty(char *array,int vc){
 	for(i = 0;i<vc;i++){
 		array[i] = ' ';
 	}
-}
-
-int is_not_empty(char *set,int vc){
-	int i = 0;
-	for(i = 0;i<vc;i++){
-		if(set[i] != ' '){
-			return 1;
-		}
-	}
-	return 0;
-}
-
-char get_next_vertex(char *array,int vc){
-	int i = 0;
-	for(i = 0;i<vc;i++){
-		if(array[i] != ' '){
-			return array[i];
-		}
-	}
-	return ' ';
 }
 
 void add_to_set(char *set,char vertex,int vc){
@@ -136,55 +129,35 @@ void remove_from_set(char *set,char vertex,int vc){
 	}
 }
 
-void print_set(char *set,int vc){
+void print_set(char *set_name,char *set,int vc){
 	int i = 0;
 	char s;
+	printf("%s ",set_name);
 	for(i = 0;i<vc;i++){
 		char s = set[i];
-		if(s == ' '){
-			s = '@';
-		}
-		printf("%c\t",s);
+		printf("%c,",s);
 	}
 	printf("\n");
 }
 
-void prims(struct edge *edges,int vc,int ec){
-	char *reached_set = (char *)malloc(sizeof(char) * vc);
-	char *unreached_set = (char *)malloc(sizeof(char) * vc);
-	struct edge *sparse_matrix = (struct edge *)malloc(sizeof(struct edge *)*ec);
-	struct edge *edges_of_vertex;
-	struct edge candidate_edge;
-	int no_of_edges_of_given_vertex;
-	int pass = 0,pos = 0,i = 0;
-	int size_of_unreached = vc,size_of_reached = 0,size_to_calculate;
-	char next_vertex;
-	clrscr();
-	fill(unreached_set,vc);
-	empty(reached_set,vc);
-	print_edges(edges,ec);
-	getch();
+struct edge* find_distance(char src,char dst,struct edge* edges,int ec,int INFINITE){
+	int i = 0;
+	int cost = INFINITE;
+	struct edge* edge = NULL;
+	edge->cost = INFINITE;
 
-	while(pass < 2){
-		next_vertex = VERTEX_START;
-		remove_from_set(unreached_set,next_vertex,vc);
-		add_to_set(reached_set,next_vertex,vc);
-		print_set(unreached_set,vc);
-		print_set(reached_set,vc);
-		size_of_reached = size_of_reached + 1;
-		size_of_unreached = size_of_unreached - 1;
-		size_to_calculate = size_of_reached + size_of_unreached;
-		edges_of_vertex = (struct edge *)malloc(sizeof(struct edge *) * size_to_calculate);
-
-
-
-
-		pass = pass + 1;
+	for(i = 0;i<ec;i++){
+		if(edges[i].src == src && edges[i].dst == dst){
+			edge = &edges[i];
+			break;
+		}
+		if(edges[i].src == dst && edges[i].dst == src){
+			edge = &edges[i];
+			break;
+		}
 	}
-
-
-	printf("Final Sparse Matrix \n");
-	print_edges(sparse_matrix,pos);
+	printf("%c - %c %d %d\n",src,dst,cost,edge->cost);
+	return edge;
 }
 
 void test_case1(){
@@ -247,8 +220,33 @@ void test_case2(){
 	prims(edges,vc,ec);
 }
 
+void test_case3(){
+	//https://lcm.csa.iisc.ernet.in/ds
+	int vc = 6;
+	int ec = 10;
+
+	struct edge *edges = (struct edge *)malloc(sizeof(struct edge) * ec);
+	VERTEX_START = '1';VERTEX_END = '6';
+	edges[0].src = '1'; edges[0].dst = '2'; edges[0].cost = 6;
+	edges[1].src = '1'; edges[1].dst = '3'; edges[1].cost = 1;
+	edges[2].src = '1'; edges[2].dst = '4'; edges[2].cost = 5;
+
+	edges[3].src = '2'; edges[3].dst = '3'; edges[3].cost = 5;
+	edges[4].src = '2'; edges[4].dst = '5'; edges[4].cost = 3;
+
+	edges[5].src = '3'; edges[5].dst = '5'; edges[5].cost = 6;
+	edges[6].src = '3'; edges[6].dst = '6'; edges[6].cost = 4;
+
+	edges[7].src = '4'; edges[7].dst = '6'; edges[7].cost = 2;
+	edges[8].src = '5'; edges[8].dst = '6'; edges[8].cost = 6;
+	edges[8].src = '3'; edges[8].dst = '4'; edges[8].cost = 5;
+
+	prims(edges,vc,ec);
+}
+
+
 int main(){
-	test_case1();
+	test_case3();
 	getch();
 	return 1;
 }
