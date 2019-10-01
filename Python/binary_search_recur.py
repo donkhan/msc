43fef7eac984b537f113d12375b57dd4c54f5635
donkhan <kamilkhan@rediffmail.com>
2019-10-01 16:33:10 +0530

    made to take input

--- Python/binary_search_recur.py
+++ Python/binary_search_recur.py
@@ -6,6 +6,7 @@ Created on Tue Oct  1 15:34:21 2019
 """
 
 def bs(l,ele):
+    print("Searching in " + str(l))
     mid = len(l) // 2
     if l[mid] == ele:
         return True
@@ -18,10 +19,12 @@ def bs(l,ele):
     
     
 def main():
-    print(bs([1,2,3,4,5],15))
-    print(bs([1,2,3,4,5],5))
-    print(bs([1,2,3,4,5],1))
-    print(bs([1,2,3,4,5],2))
-    print(bs([1,2,3,4,5],-22))
+    l = [1,2,3,4,5]
+    print("List is " + str(l))
+    found = bs(l,int(input("Enter the Value to Search: ")))
+    if found is True:
+        print("Found...")
+    else:
+        print("Not Found...")
 
 main()
