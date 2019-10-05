018081f2232bc74ea6c5dc171051a2fed5ae5332
kamilkhan@rediffmail.com <donkhan>
2019-10-05 18:04:48 +0530

    Added function pointers to point test case

--- DS/GraphTheory/Dijikstra.c
+++ DS/GraphTheory/Dijikstra.c
@@ -292,6 +292,9 @@ void test_case7(){
 }
 
 int main(){
-	test_case7();
-	//getch();
+	int i;
+	void (*fun_ptrs[])() = {test_case1,test_case2,test_case3,test_case4,test_case5,test_case6,test_case7}; 
+	printf("Enter Test Case No .......");
+	scanf("%d",&i);
+	fun_ptrs[i-1]();
 }
