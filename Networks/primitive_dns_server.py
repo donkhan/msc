7641c26d26751667d4eb7566f65cfb8f0d17551d
donkhan <kamilkhan@rediffmail.com>
2020-02-03 15:56:48 +0530

    dns server and client added

--- Networks/primitive_dns_server.py
+++ Networks/primitive_dns_server.py
@@ -6,11 +6,22 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_address = ('localhost', 10000)
 print('server starting up... ')
 sock.bind(server_address)
+map = {}
+
+f = open("dns_map.txt")
+for line in f:
+    line = line.replace("\n","")
+    tokens = line.split("=")
+    map[tokens[0]] = tokens[1]
+
 
 while True:
     print('\nwaiting to receive message')
     data, address = sock.recvfrom(4096)
     print("Length of Data = ", len(data)," Data = ",data.decode(), "From = ", address)
-    if data:
-        sent = sock.sendto(data, address)
-        print('Sent ',sent,' bytes back to ' ,address)
+
+    response = map.get(data.decode())
+    if response is None:
+        response = " Host name not found for " + data.decode()
+    sent = sock.sendto(response.encode(), address)
+    print('Sent ',sent,' bytes back to ' ,address)
