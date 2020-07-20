30fe1c6f619957a72f53c70ec5f8b29b2e058f93
donkhan <kamilkhan@rediffmail.com>
2020-07-20 15:43:10 +0530

    make file is made

---
+++ Network-Security/include/caesar.h
@@ -0,0 +1,12 @@
+/*
+ * caesar.h
+ *
+ *  Created on: 20-Jul-2020
+ *      Author: kkhan
+ */
+
+
+
+char* encryption(char* plain_text,int k);
+char* decryption(char* cipher_text,int k);
+
--- Network-Security/src/Caesar.c
+++ Network-Security/src/Caesar.c
@@ -5,10 +5,10 @@
  *      Author: kkhan
  */
 
-
 #include<string.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include "caesar.h"
 
 char* encrypt(char* plain_text,int k){
 	char* cipher_text = (char *)malloc(sizeof(char) * strlen(plain_text));
@@ -28,26 +28,6 @@ char* decrypt(char* cipher_text,int k){
 	return plain_text;
 }
 
-
-void test_encryption(){
-	printf("%s %s\n", "ABC",encrypt("ABC",3));
-	printf("%s %s\n", "XYZ",encrypt("XYZ",3));
-	printf("%s %s\n", "HELLOW WORLD",encrypt("HELLOW WORLD",3));
-}
-
-
-void test_decryption(){
-	printf("%s %s\n", "DEF",decrypt("DEF",3));
-}
-
-void test(){
-	char* C = encrypt("HELLOWWORLD",3);
-	printf("Cipher Text %s\n",C);
-	char* P = decrypt(C,3);
-	printf("Plain Text %s\n",P);
-
-}
-
 int main(){
 	char i[10],*p,*c;
 	int k;
@@ -59,5 +39,5 @@ int main(){
 	printf("Cipher Text %s\n",c);
 	p = decrypt(c,k);
 	printf("Plain Text %s\n",p);
-
 }
+
